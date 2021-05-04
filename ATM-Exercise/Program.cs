561a8dcc9a4039ba4c5d99e4763da06d1caaa8c0
kevkouadio <kevkouadio@gmail.com>
2021-05-04 11:54:06 -0400

    fix of changed account balance after withdrawal

--- ATM-Exercise/Program.cs
+++ ATM-Exercise/Program.cs
@@ -7,19 +7,19 @@ namespace ATM_Exercise
         static void Main(string[] args)
         {
             double accountBalance, withdrawal, bankFees, newAccountBalance;
+            accountBalance = 500.00d;
             bool moreTransactions = true;
+           
             while (moreTransactions)
             {
                 try
                 {
-                    accountBalance = 500.00d;
-                    
                     Console.WriteLine("Enter the amount you wish to withdraw: ");
 
                     withdrawal = Convert.ToDouble(Console.ReadLine());
                     bankFees = +0.50d;
                     newAccountBalance = accountBalance - withdrawal - bankFees;
-
+                   
                     if (withdrawal > accountBalance)
                     {
                         Console.WriteLine("insufficent fund in account balance");
@@ -28,6 +28,7 @@ namespace ATM_Exercise
                     else if (withdrawal % 5 == 0)
                     {
                         Console.WriteLine("Your Balance after your operation is : $" + newAccountBalance);
+                        accountBalance = newAccountBalance;
                     }
                     else if (withdrawal % 5 != 0)
                     {
@@ -40,7 +41,10 @@ namespace ATM_Exercise
                     if (ans == "N")
                         moreTransactions = false;
                     else
+                    {
                         Console.WriteLine("Please enter dollar amount in multiples of 5: ");
+                    }
+                   
                 }
                 catch (Exception)
                 {
@@ -50,15 +54,16 @@ namespace ATM_Exercise
                     string ans = Console.ReadLine().ToUpper();
                     if (ans == "N")
                         moreTransactions = false;
-                    else
+
+                    else 
+                    {
                         Console.WriteLine("Please enter dollar amount in multiples of 5: ");
-                    //return;
+                    }
+                    
                 }
                 
             }
             
-
-
         }
     }
 }
