45d1c7c85863655e018b9e7725e5fcb00635ef54
kevkouadio <kevkouadio@gmail.com>
2021-05-04 14:53:34 -0400

    fix of more transaction bug

--- ATM-Exercise/Program.cs
+++ ATM-Exercise/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 
+
+
+
 namespace ATM_Exercise
 {
     class Program
@@ -9,59 +12,56 @@ namespace ATM_Exercise
             double accountBalance, withdrawal, bankFees, newAccountBalance;
             accountBalance = 500.00d;
             bool moreTransactions = true;
-           
+
             while (moreTransactions)
             {
                 try
                 {
-                    Console.WriteLine("Enter the amount you wish to withdraw: ");
+                    Console.WriteLine("Enter the amount you wish to withdraw in multiples of 5: ");
 
                     withdrawal = Convert.ToDouble(Console.ReadLine());
                     bankFees = +0.50d;
                     newAccountBalance = accountBalance - withdrawal - bankFees;
-                   
-                    if (withdrawal > accountBalance)
+
+                    if (withdrawal >= accountBalance)
                     {
                         Console.WriteLine("insufficent fund in account balance");
                     }
 
                     else if (withdrawal % 5 == 0)
                     {
+                        Console.WriteLine("Successful operation!");
                         Console.WriteLine("Your Balance after your operation is : $" + newAccountBalance);
                         accountBalance = newAccountBalance;
                     }
                     else if (withdrawal % 5 != 0)
                     {
-                        Console.WriteLine("Please enter a correct amount: ");
+                        Console.WriteLine("Please enter a correct amount in multiples of 5: ");
                         Console.ReadLine();
                         Console.WriteLine("Your Balance after your operation is : $" + newAccountBalance);
                     }
-                    Console.WriteLine("Would you like to conduct another transaction? (Y/N)");
-                    string ans = Console.ReadLine().ToUpper();
-                    if (ans == "N")
-                        moreTransactions = false;
-                    else
-                    {
-                        Console.WriteLine("Please enter dollar amount in multiples of 5: ");
-                    }
-                   
+
                 }
                 catch (Exception)
                 {
-                    Console.WriteLine("Please enter dollar amount in multiples of 5: ");
-
-                    Console.WriteLine("Would you like to conduct another transaction? (Y/N)");
-                    string ans = Console.ReadLine().ToUpper();
-                    if (ans == "N")
-                        moreTransactions = false;
-
-                    else 
-                    {
-                        Console.WriteLine("Please enter dollar amount in multiples of 5: ");
-                    }
-                    
+                    Console.WriteLine("Incorrect amount!");
                 }
                 
+                Console.WriteLine("Would you like to conduct another transaction? (Y/N)");
+                string ans = Console.ReadLine().ToUpper();
+    
+                if (ans == "N")
+                    moreTransactions = false;
+                else if (ans == "Y")
+                {
+                    moreTransactions = true;
+                    //Console.WriteLine("Please enter dollar amount in multiples of 5: ");
+                }
+                else
+                {
+                    Console.WriteLine("Please enter 'Y' for Yes or 'N' for No:");
+                    _ = Console.ReadLine().ToUpper();
+                }
             }
             
         }
